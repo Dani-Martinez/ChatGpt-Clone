3f9271e4a8ff6bc732a46e366acd62983e601851
Daniela Martinez <dmartinez@nevada.unr.edu>
2023-07-05 19:08:51 -0700

    Layout for CHATGPT Created
    Basic Layout of how chatgpt looks

---
+++ react-chatgpt-clone/src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>[MADE BY DANIELA M.]</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  )
+}
+
+export default App
